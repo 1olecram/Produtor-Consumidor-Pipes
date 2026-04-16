cbd23be6c483069496eea175020ef05d968919b7
1olecram <marcelochave059@gmail.com>
2026-04-16 20:51:04 -0300

    feat: logica do numero primo e definicao do buffer size

--- pipes.cpp
+++ pipes.cpp
@@ -4,22 +4,50 @@
 #include <random>
 #include <vector>
 
-std::vector<int> numGeneration(int genNums){
-    std::vector<int> numsToRead;
-    // 1. Seed with a real hardware entropy source
-    std::random_device rd; 
-    // 2. Initialize a generator (Mersenne Twister is standard)
-    std::mt19937 gen(rd()); 
-    // 3. Define a range (e.g., integers between 1 and 100)
-    std::uniform_int_distribution<> distrib(1, 100); 
+bool isPrime(int num)
+{
+    int cnt = 0;
+    if (num <= 1)
+    {
+        return false;
+    }
+    else
+    {
+        // Conta os divisores n considerando o quadrados desses numeros -> O(sqrt(n))
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+                cnt++;
+        }
+        // Se n e divisivel por mais de 2
+        if (cnt > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
 
+std::vector<int> numGeneration(int genNums)
+{
+    std::vector<int> numsToRead;
+    // Seed com fonte de entropia via hardware
+    std::random_device rd;
+    // Inicializacao do gerador Mersenne Twister (standard)
+    std::mt19937 gen(rd());
+    //∆ ∈ [1, 100]
+    std::uniform_int_distribution<> distrib(1, 100);
+
+    //N0 = 1
     int current = 1;
     numsToRead.push_back(current);
-    std::cout << current << std::endl; // Pai exibe 1 (o primeiro número)
 
     for (int i = 1; i < genNums; i++)
     {
-        // Ni = Ni-1 + ∆, N0 = 1, ∆ ∈ [1, 100].
+        // Ni = Ni-1 + ∆
         current += distrib(gen);
         numsToRead.push_back(current);
     }
@@ -28,49 +56,56 @@ std::vector<int> numGeneration(int genNums){
     return numsToRead;
 }
 
-int main() {
+int main()
+{
+    const int BUFFER_SIZE = 1000;
     int fd[2];
-    
+
     // Cria o pipe. Se retornar -1, houve um erro.
-    if (pipe(fd) == -1) return 1;
+    if (pipe(fd) == -1)
+        return 1;
 
     pid_t pid = fork();
 
-    if (pid < 0) return 1;
+    if (pid < 0)
+        return 1;
 
-    if (pid == 0) {
+    if (pid == 0)
+    {
         // Código do Processo Filho
-        close(fd[1]); 
-        
+        close(fd[1]);
+
         // Agora 'buffer' é um array de inteiros
-        int buffer[10]; 
-        
+        int buffer[BUFFER_SIZE];
+
         // Lê os bytes do pipe diretamente para o array de inteiros
         ssize_t read_bytes = read(fd[0], buffer, sizeof(buffer));
-        
+
         // Descobre quantos números (inteiros) foram lidos
         int num_elements = read_bytes / sizeof(int);
-        
+
         std::cout << "Child received: " << std::endl;
-        for (int i = 0; i < num_elements; i++)
+        for (int i = 0; i < num_elements && buffer[i] != 0; i++)
         {
 
-            std::cout << buffer[i] << std::endl;
+            std::cout << buffer[i] << (isPrime(buffer[i]) ? " e primo" : " nao e primo") << std::endl;
         }
-        
+
         close(fd[0]);
-    } else {
+    }
+    else
+    {
         // Código do Processo Pai
-        close(fd[0]); 
-        
-        // Gera os números
-        std::vector<int> nums = numGeneration(4);
-        
+        close(fd[0]);
+
+        // Gera os números para caber no buffer (considerando o 0 no final)
+        std::vector<int> nums = numGeneration(BUFFER_SIZE - 1);
+
         // Escreve os inteiros diretamente no pipe
         write(fd[1], nums.data(), nums.size() * sizeof(int));
-        
+
         close(fd[1]);
-        wait(NULL); 
+        wait(NULL);
     }
     return 0;
 }
